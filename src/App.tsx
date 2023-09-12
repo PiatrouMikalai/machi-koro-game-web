70811313990e95ccc13a7b6c9824e4c5c7d54c2c
Mikalai_Piatrou <piatrou.mikalai@gmail.com>
2023-09-12 15:32:47 +0300

    Add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import * as S from './App.styled';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <S.App>
+      <S.AppHeader>
+        <S.AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <S.AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </S.AppLink>
+      </S.AppHeader>
+    </S.App>
   );
 }
 
