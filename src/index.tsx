18fa1f8116c8a7ce2f200590f9cea0f0942e1f22
Mikalai_Piatrou <piatrou.mikalai@gmail.com>
2023-09-12 15:42:18 +0300

    Add react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { store } from './store/store'
 import App from './App';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
